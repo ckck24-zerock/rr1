fff906e56635241220d315172ca5648b92922932
zerock <kangckck24@gmail.com>
2025-04-11 11:35:59 +0900

    zustand config

--- app/routes/todo/listPage.tsx
+++ app/routes/todo/listPage.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import TodoListComponent from "~/components/todo/listComponent";
 
 function TodoListPage() {
     return (
         <div>
             <div className={'text-4xl'}>Todo List Page</div>
+
+            <TodoListComponent></TodoListComponent>
+
         </div>
     );
 }
