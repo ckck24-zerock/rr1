7662059a4025d5204a10341b0ff3e57c81b1336a
zerock <kangckck24@gmail.com>
2025-04-15 14:27:51 +0900

    zustand config

--- app/api/kakaoAPI.ts
+++ app/api/kakaoAPI.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 
 
 const rest_api_key:string ='580d11f3088c34e1b2f1115b3269a2e9'
@@ -5,9 +6,32 @@ const redirect_uri = 'http://localhost:5173/member/kakao'
 
 const auth_code_path = `https://kauth.kakao.com/oauth/authorize`
 
+const access_token_url = 'https://kauth.kakao.com/oauth/token'
+
 export const getKakaoLoginLink = () => {
 
     const kakaoURL = `${auth_code_path}?client_id=${rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`
 
     return kakaoURL
+}
+
+export const getAccessToken = async (authCode:string) => {
+
+    const header = {
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        }
+    }
+    const params = {
+        grant_type: "authorization_code",
+        client_id: rest_api_key,
+        redirect_uri: redirect_uri,
+        code:authCode
+    }
+
+    const res = await axios.post(access_token_url, params , header)
+
+    const accessToken = res.data.access_token
+
+    return accessToken
 }
