65114ddc71db2c3d9bdc573633a3df46dc8e6f1d
zerock <kangckck24@gmail.com>
2025-04-15 13:53:45 +0900

    zustand config

---
+++ app/routes/member/loginPage.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Link} from "react-router";
+import {getKakaoLoginLink} from "~/api/kakaoAPI";
+
+function LoginPage() {
+
+    const kakaoLink = getKakaoLoginLink()
+
+    return (
+        <div>
+            <div className={'text-4xl'}>Login Page</div>
+
+            <Link to={kakaoLink}>카카오</Link>
+
+        </div>
+    );
+}
+
+export default LoginPage;
